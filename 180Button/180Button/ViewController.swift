3ca6dfc2fa65bd3b141148da96d834b2c843f9ce
Nathalia <nathaliamelare@gmail.com>
2020-06-01 21:28:48 -0300

    Create button programmatically

--- 180Button/180Button/ViewController.swift
+++ 180Button/180Button/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpButton()
     }
 
+    func setUpButton() {
+           let button = UIButton(type: .system)
+           button.frame = CGRect(x: self.view.frame.width/3, y: self.view.frame.height/2, width: 150, height: 75)
+           button.backgroundColor = .systemGreen
+           button.setTitle("Call", for: UIControl.State.normal)
+           button.setTitleColor(.white, for: UIControl.State.normal)
+           button.titleLabel?.font = .systemFont(ofSize: 40)
+           self.view.addSubview(button)
+       }
 
 }
 
